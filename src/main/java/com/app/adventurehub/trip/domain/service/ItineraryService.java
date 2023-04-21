193c172510c94ca0e12ac316371808356ef2bf17
Jasmin Pomasoncco <89169379+jasminpomasoncco@users.noreply.github.com>
2023-04-20 20:00:39 -0500

    update

--- src/main/java/com/app/adventurehub/trip/domain/service/ItineraryService.java
+++ src/main/java/com/app/adventurehub/trip/domain/service/ItineraryService.java
@@ -1,2 +1,16 @@
-package com.app.adventurehub.trip.domain.service;public interface ItineraryService {
+package com.app.adventurehub.trip.domain.service;
+
+import com.app.adventurehub.trip.domain.model.entity.Itinerary;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface ItineraryService {
+    List<Itinerary> getAll();
+    Itinerary getById(Long itineraryId);
+    Itinerary create(Itinerary itinerary);
+    Itinerary update(Long id, Itinerary itinerary);
+    ResponseEntity<?> delete(Long itineraryId);
+
+    Itinerary addTripToItinerary(Long itineraryId, String tripName);
 }
--- src/main/java/com/app/adventurehub/trip/service/ItineraryServiceImpl.java
+++ src/main/java/com/app/adventurehub/trip/service/ItineraryServiceImpl.java
@@ -1,2 +1,39 @@
-package com.app.adventurehub.trip.service;public class Itinerary {
+package com.app.adventurehub.trip.service;
+
+import com.app.adventurehub.trip.domain.model.entity.Itinerary;
+import com.app.adventurehub.trip.domain.service.ItineraryService;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class ItineraryServiceImpl implements ItineraryService {
+    @Override
+    public List<Itinerary> getAll() {
+        return null;
+    }
+
+    @Override
+    public Itinerary getById(Long itineraryId) {
+        return null;
+    }
+
+    @Override
+    public Itinerary create(Itinerary itinerary) {
+        return null;
+    }
+
+    @Override
+    public Itinerary update(Long id, Itinerary itinerary) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> delete(Long itineraryId) {
+        return null;
+    }
+
+    @Override
+    public Itinerary addTripToItinerary(Long itineraryId, String tripName) {
+        return null;
+    }
 }
