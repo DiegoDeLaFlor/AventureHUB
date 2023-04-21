4e00f7c6e0bd0847fe5c2710519f02fa3d90b670
Jasmin Pomasoncco <89169379+jasminpomasoncco@users.noreply.github.com>
2023-04-20 19:15:03 -0500

    update

---
+++ src/main/java/com/app/adventurehub/trip/domain/model/entity/Itinerary.java
@@ -0,0 +1,27 @@
+package com.app.adventurehub.user.domain.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Itinerary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
+    private String name;
+    private String description;
+    private String location;
+    private String latitude;
+    private String longitude;
+    @NotNull
+    @NotBlank
+    private int day;
+}
---
+++ src/main/java/com/app/adventurehub/trip/domain/model/entity/TripDetails.java
@@ -0,0 +1,24 @@
+package com.app.adventurehub.user.domain.model.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class TripDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
+    private String imageUrl;
+    private String difficulty;
+    private String duration;
+    private String group_size;
+    private String activities;
+    private String accommodation;
+}
